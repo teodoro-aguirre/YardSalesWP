4cf29e204750a3cae4899d69c45a0e0a735e222f
Teodoro Rodríguez Aguirre <teodororoag@MacBook-Pro-de-Teodoro.local>
2023-02-11 00:59:41 -0600

    Se aprendió a crear un custom post type y las caracteristicas que incluyen, y que significan cada una de estas

--- wp-content/themes/yardsales/functions.php
+++ wp-content/themes/yardsales/functions.php
@@ -46,3 +46,69 @@ function plz_add_menus(){
 }
 
 add_action("after_setup_theme", "plz_add_menus");
+
+
+// Registramos un sidebar para nuestros widgets
+function plz_add_sidebar(){
+    register_sidebar(
+        array(
+            'name' => 'Pie de página',
+            'id' => 'pie-pagina',
+            'before-widget' => '',
+            'after-widget' => ''
+        )
+        );
+}
+
+add_action("widgets_init", "plz_add_sidebar");
+
+function plz_add_custom_post_type(){
+    $labels = array(
+		'name'                  => _x( 'Producto', 'Post type general name', 'textdomain' ),
+		'singular_name'         => _x( 'Producto', 'Post type singular name', 'textdomain' ),
+		'menu_name'             => _x( 'Productos', 'Admin Menu text', 'textdomain' ),
+		'name_admin_bar'        => _x( 'Productos', 'Add New on Toolbar', 'textdomain' ),
+		'add_new'               => __( 'Añadir producto', 'textdomain' ),
+		'add_new_item'          => __( 'Añadir nuevo producto', 'textdomain' ),
+		'new_item'              => __( 'Nuevo producto', 'textdomain' ),
+		'edit_item'             => __( 'Editar producto', 'textdomain' ),
+		'view_item'             => __( 'Ver producto', 'textdomain' ),
+		'all_items'             => __( 'Todos los productos', 'textdomain' ),
+		'search_items'          => __( 'Buscar productos', 'textdomain' ),
+		'not_found'             => __( 'Productos no encontrados', 'textdomain' ),
+		'not_found_in_trash'    => __( 'Productos no encontrados en la papelera.', 'textdomain' ),
+		'featured_image'        => _x( 'Imagen del producto', 'Overrides the “Featured Image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'set_featured_image'    => _x( 'Elegir imagen del producto', 'Overrides the “Set featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'remove_featured_image' => _x( 'Remover imagen del producto', 'Overrides the “Remove featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'use_featured_image'    => _x( 'Usar como imagen del producto', 'Overrides the “Use as featured image” phrase for this post type. Added in 4.3', 'textdomain' ),
+		'archives'              => _x( 'Productos archivo', 'The post type archive label used in nav menus. Default “Post Archives”. Added in 4.4', 'textdomain' ),
+		'insert_into_item'      => _x( 'Insertar en el producto', 'Overrides the “Insert into post”/”Insert into page” phrase (used when inserting media into a post). Added in 4.4', 'textdomain' ),
+		'uploaded_to_this_item' => _x( 'Actualizar producto', 'Overrides the “Uploaded to this post”/”Uploaded to this page” phrase (used when viewing media attached to a post). Added in 4.4', 'textdomain' ),
+		'filter_items_list'     => _x( 'Lista de productos filtrados', 'Screen reader text for the filter links heading on the post type listing screen. Default “Filter posts list”/”Filter pages list”. Added in 4.4', 'textdomain' ),
+		'items_list_navigation' => _x( 'Lista de navegación de productos', 'Screen reader text for the pagination heading on the post type listing screen. Default “Posts list navigation”/”Pages list navigation”. Added in 4.4', 'textdomain' ),
+		'items_list'            => _x( 'Lista de Productos', 'Screen reader text for the items list heading on the post type listing screen. Default “Posts list”/”Pages list”. Added in 4.4', 'textdomain' ),
+	
+	);
+    $args = array(
+        'labels'             => $labels,
+        'description'        => 'Productos para listar en un catalogo',
+        'public'             => true,
+		'publicly_queryable' => true,
+		'show_ui'            => true,
+		'show_in_menu'       => true,
+		'query_var'          => true,
+		'rewrite'            => array( 'slug' => 'producto' ),
+		'capability_type'    => 'post',
+		'has_archive'        => true,
+		'hierarchical'       => false,
+		'menu_position'      => 5,
+		'supports'           => array( 'title', 'editor', 'author', 'thumbnail'),
+        'taxonomies'         => array('category'),
+        'show_in_rest'       => true,
+        'menu_icon'          => 'dashicons-cart',
+	);
+    
+    register_post_type('producto', $args);
+}
+
+add_action("init", "plz_add_custom_post_type");
