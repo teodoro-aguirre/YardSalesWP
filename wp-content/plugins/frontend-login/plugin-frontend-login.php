999c0253c0e1f3474f285bfee2dcdc4f6c499d45
Teodoro Rodriguez Aguirre <rodriguezaguirreangelt@gmail.com>
2023-02-12 17:55:14 -0600

    Vimos como incorporar un archivo a nuestro plugin con codigo y funcionalidades y tambien que es un shortcode y como crearlo

--- wp-content/plugins/frontend-login/plugin-frontend-login.php
+++ wp-content/plugins/frontend-login/plugin-frontend-login.php
@@ -3,3 +3,7 @@
 /*
  * Plugin Name: Frontend Login
 */
+
+require_once plugin_dir_path(__FILE__)."public/shortcode/form-registro.php";
+
+require_once plugin_dir_path(__FILE__)."public/shortcode/form-login.php";
---
+++ wp-content/plugins/frontend-login/public/shortcode/form-login.php
@@ -0,0 +1,29 @@
+<?php 
+
+function plz_add_login_form(){
+ $response = '
+ <div class="signin">
+        <div class="signin__container">
+            <form class="signin__form">
+                <div class="signin__email name--campo">
+                    <label for="email">Email address</label>
+                    <input type="email" id="email">
+                </div>
+                <div class="signin__pass name--campo">
+                    <label for="password">Password</label>
+                    <input type="password" id="password">
+                </div>
+                <div class="signin__submit">
+                    <input type="submit" value="Log in">
+                </div>
+                <div class="signin_create-link">
+                    <a href="/sign_up.html">Sign up</a>
+                </div>
+            </form>
+        </div>
+    </div>';
+
+    return $response;
+}
+
+add_shortcode("plz_login", "plz_add_login_form");
